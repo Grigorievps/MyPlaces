6841e44aed224b637a7744b47f3f161604e16f85
Grigoriev Pavel <p.s.grigoriev@yandex.ru>
2020-01-13 17:16:31 +0300

    Initial Commit

---
+++ MyPlaces/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyPlaces
+//
+//  Created by Павел Григорьев on 13.01.2020.
+//  Copyright © 2020 GP. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
