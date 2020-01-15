4c8897c6ab3c97e0f6ef972bf0b59a2a37861fdc
Grigoriev Pavel <p.s.grigoriev@yandex.ru>
2020-01-15 10:56:08 +0300

    Custom TableViewCell

---
+++ MyPlaces/CustomTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaces
+//
+//  Created by Павел Григорьев on 15.01.2020.
+//  Copyright © 2020 GP. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var locationLable: UILabel!
+    @IBOutlet weak var typeLable: UILabel!
+}
